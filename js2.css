88dbc82fe14bb4c86e192f8460e17c8590a26d51
Tavares0909 <101602902+Samara0909@users.noreply.github.com>
2022-08-22 17:19:21 -0300

    Create js2.css

---
+++ js2.css
@@ -0,0 +1,12 @@
+@import url('https://fonts.googleapis.com/css2?family=Macondo&display=swap');
+
+body{
+    font-family: arial;
+    background-color: blue;
+    margin: 7%;
+    font-size: 40px;
+}
+
+h2{
+    padding-top: 160px;
+}
